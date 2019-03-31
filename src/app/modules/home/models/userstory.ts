cbf0dbc5f9b9b9a9274ab0cfb89cfba3c0848d3d
Buwaneka Thilakasiri <buwanekathilakasiri@gmail.com>
2019-03-31 08:55:26 +0530

    Changing project

--- src/app/modules/home/models/userstory.ts
+++
@@ -1,9 +0,0 @@
-export class UserStory { 
-    id: number;
-    description: string;
-    status: string;
-    stepsJson: string;
-    url: string;
-    keywords: string
-    acceptanceCriteria: string
-}
